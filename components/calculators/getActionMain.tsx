33a4982e348ca92ac12de54823ef0a1579656b3f
Val Rakitine <vrakitine@gmail.com>
2023-11-08 19:55:13 -0500

    Added ./actionFunctions

--- components/calculators/getActionMain.tsx
+++ components/calculators/getActionMain.tsx
@@ -1,4 +1,5 @@
 import vaScript from "../../public/vaop/va-scripts/vaScriptBase10_v1.json";
+import { actionFunctions } from "./actionFunctions";
 import {Text,VStack,HStack,Stack,} from "@chakra-ui/react";
 import { useState, useEffect} from "react";
 import { VaScriptAction } from "../../types/types";
@@ -7,51 +8,6 @@ import { ActionMap } from "../../types/types";
 import { ActionMapping } from "../../types/types";
 import ActionList from "./ActionList";
 import ActionButton from "./ActionButton";
-import Action_show_result from "../../public/Actions/Action_show_result";
-import Action_clear from "../../public/Actions/Action_clear";
-import Action_init from "../../public/Actions/Action_init";
-import Action_operand_1_attach_zero from "../../public/Actions/Action_operand_1_attach_zero";
-import Action_operand_1_attach_one from "../../public/Actions/Action_operand_1_attach_one";
-import Action_operand_1_attach_two from "../../public/Actions/Action_operand_1_attach_two";
-import Action_operand_1_attach_three from "../../public/Actions/Action_operand_1_attach_three";
-import Action_operand_1_attach_four from "../../public/Actions/Action_operand_1_attach_four";
-import Action_operand_1_attach_five from "../../public/Actions/Action_operand_1_attach_five";
-import Action_operand_1_attach_six from "../../public/Actions/Action_operand_1_attach_six";
-import Action_operand_1_attach_seven from "../../public/Actions/Action_operand_1_attach_seven";
-import Action_operand_1_attach_eight from "../../public/Actions/Action_operand_1_attach_eight";
-import Action_operand_1_attach_nine from "../../public/Actions/Action_operand_1_attach_nine";
-import Action_waiting_for_operand_2_for_plus from "../../public/Actions/Action_waiting_for_operand_2_for_plus";
-import Action_operand_2_attach_zero from "../../public/Actions/Action_operand_2_attach_zero";
-import Action_operand_2_attach_one from "../../public/Actions/Action_operand_2_attach_one";
-import Action_operand_2_attach_two from "../../public/Actions/Action_operand_2_attach_two";
-import Action_operand_2_attach_three from "../../public/Actions/Action_operand_2_attach_three";
-import Action_operand_2_attach_four from "../../public/Actions/Action_operand_2_attach_four";
-import Action_operand_2_attach_five from "../../public/Actions/Action_operand_2_attach_five";
-import Action_operand_2_attach_six from "../../public/Actions/Action_operand_2_attach_six";
-import Action_operand_2_attach_seven from "../../public/Actions/Action_operand_2_attach_seven";
-import Action_operand_2_attach_eight from "../../public/Actions/Action_operand_2_attach_eight";
-import Action_operand_2_attach_nine from "../../public/Actions/Action_operand_2_attach_nine";
-import Action_warning_10__Second_operand_is_missing from "../../public/Actions/Action_warning_10__Second_operand_is_missing";
-import { callbackify } from "util";
-
-// interface getActionProps {
-//     direction: Direction;
-//     currentAction: string;
-//     setCurrentAction: React.Dispatch<React.SetStateAction<string>>;
-//     previousAction: string;
-//     setPreviousAction: React.Dispatch<React.SetStateAction<string>>;
-//     directionAction: string;
-//     setDirectionAction: React.Dispatch<React.SetStateAction<string>>;
-//     operandOne: string;
-//     setOperandOne: React.Dispatch<React.SetStateAction<string>>;
-//     operandTwo: string;
-//     setOperandTwo: React.Dispatch<React.SetStateAction<string>>;
-//     result: string;
-//     setResult: React.Dispatch<React.SetStateAction<string>>;
-//     warningMsg: string;
-//     setWarningMsg: React.Dispatch<React.SetStateAction<string>>;
-// }
-
 
 
 function getActionMain(
@@ -76,36 +32,6 @@ function getActionMain(
     console.log("[" + direction + "]");
     console.log("[" + currentAction + "]");
     
-
- 
-  const actionFunctions: Record<VaScriptAction, Function> = {
-    Action_init,
-    Action_operand_1_attach_zero,
-    Action_operand_1_attach_one,
-    Action_operand_1_attach_two,
-    Action_operand_1_attach_three,
-    Action_operand_1_attach_four,
-    Action_operand_1_attach_five,
-    Action_operand_1_attach_six,
-    Action_operand_1_attach_seven,
-    Action_operand_1_attach_eight,
-    Action_operand_1_attach_nine,
-    Action_waiting_for_operand_2_for_plus,
-    Action_operand_2_attach_zero,
-    Action_operand_2_attach_one,
-    Action_operand_2_attach_two,
-    Action_operand_2_attach_three,
-    Action_operand_2_attach_four,
-    Action_operand_2_attach_five,
-    Action_operand_2_attach_six,
-    Action_operand_2_attach_seven,
-    Action_operand_2_attach_eight,
-    Action_operand_2_attach_nine,
-    Action_warning_10__Second_operand_is_missing,
-    Action_clear,
-    Action_show_result
-  };
-
     setWarningMsg("");
 
     const nextAction = vaScript[currentAction][direction] as VaScriptAction;
