04536c58d23b5b8d26747a856c39fe4d321a7475
Val Rakitine <vrakitine@gmail.com>
2023-11-23 15:53:33 -0500

    Updated "/calculator2"

--- pages/calculator2.tsx
+++ pages/calculator2.tsx
@@ -2,9 +2,10 @@ import {Text} from '@chakra-ui/react'
 
 export default function Home() {
   return (
-  <div>
+// @ts-ignore
     <Text fontSize="35px" color="gray">
-      va-calculator (base 2) <span style={{ fontSize: "20px", color: "red" }}>coming soon</span>
+        va-calculator (base 2) coming soon
     </Text>
-  </div>);
+  );
+  
 }
