9092abb165e7bdfe01fbd1f1d8b6b88af2f07c0a
Val Rakitine <vrakitine@gmail.com>
2023-11-23 16:59:59 -0500

    T cleanup

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,11 +5,9 @@ export default function Home() {
 	return (
     // @ts-ignore
   <div>
-      <Text>Ищу ассистента для верстки ReactJS/Chakra новой версии va-calculator-v1.0, которая будет использоваться в моей статье на <Link href="https://vc.ru/u/1369846-valeri-rakitine/573408-vaop-v-primerah-reactjs-mini-va-calculator">[ссылка на статью]</Link>.</Text>
-      <Text>&nbsp;</Text>
-      <Text>Предлагаю совместно работать над этим калькулятором. Вам будет предоставлено бесплатное менторство от меня, и у вас будет возможность получить опыт, который поможет вам в будущем в поиске работы.</Text>
-      <Text>Я нахожусь в Торонто. Мой подход к разработке калькулятора представлен в <Link href="https://drive.google.com/file/d/1DQ9U4dWI2uQM4nBpYycgD4v-wgvjJMGS/view">[этом видео]</Link>, которое доступно в моей группе VAOP: eco-programming paradigm <Link href="https://t.me/ecoprog">https://t.me/ecoprog</Link></Text>
-      <Text>На данный момент я сосредоточен на функциональной части. Мне нужен партнер, который добавит декоративные элементы и стили к проекту.</Text>
+      <p>Ищу ассистента для верстки ReactJS/Chakra новой версии va-calculator-v1.0, которая будет использоваться в моей статье на <a href="https://vc.ru/u/1369846-valeri-rakitine/573408-vaop-v-primerah-reactjs-mini-va-calculator">[ссылка на статью]</a>.</p>
+      <p>Предлагаю совместно работать над этим калькулятором. Вам будет предоставлено бесплатное менторство от меня, и у вас будет возможность получить опыт, который поможет вам в будущем в поиске работы.</p>      Я нахожусь в Торонто. Мой подход к разработке калькулятора представлен в <a href="https://drive.google.com/file/d/1DQ9U4dWI2uQM4nBpYycgD4v-wgvjJMGS/view">[этом видео]</a>, которое доступно в моей группе VAOP: eco-programming paradigm <a href="https://t.me/ecoprog">[https://t.me/ecoprog]</a>
+      <p>На данный момент я сосредоточен на функциональной части. Мне нужен партнер, который добавит декоративные элементы и стили к проекту.</p>  
   </div>
   );
   }
