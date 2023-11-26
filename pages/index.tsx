b6df696cec6811d183d46f15ba500d2467e67ea1
Val Rakitine <vrakitine@gmail.com>
2023-11-25 21:36:46 -0500

    Added Attention ReactJS

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,9 +5,15 @@ export default function Home() {
 	return (
     // @ts-ignore
   <div>
-      <p>___Ищу ассистента для верстки ReactJS/Chakra новой версии va-calculator-v1.0, которая будет использоваться в моей статье на <a href="https://vc.ru/u/1369846-valeri-rakitine/573408-vaop-v-primerah-reactjs-mini-va-calculator">[ссылка на статью]</a>.</p>
-      <p>Предлагаю совместно работать над этим калькулятором. Вам будет предоставлено бесплатное менторство от меня, и у вас будет возможность получить опыт, который поможет вам в будущем в поиске работы.</p>      Я нахожусь в Торонто. Мой подход к разработке калькулятора представлен в <a href="https://drive.google.com/file/d/1DQ9U4dWI2uQM4nBpYycgD4v-wgvjJMGS/view">[этом видео]</a>, которое доступно в моей группе VAOP: eco-programming paradigm <a href="https://t.me/ecoprog">[https://t.me/ecoprog]</a>
-      <p>На данный момент я сосредоточен на функциональной части. Мне нужен партнер, который добавит декоративные элементы и стили к проекту.</p>  
+      <br/><p>Attention ReactJS/Chakra Enthusiasts!</p>
+      <br/><p>Are you ready to embark on an exciting journey with VAOP's revolutionary invention, the va-calculator-v1.0? We're on the lookout for a dynamic assistant to take the reins on the ReactJS/Chakra layout of our cutting-edge calculator, soon to be featured in my groundbreaking article <a href="https://medium.com/@vrakitine/vaop-in-examples-reactjs-mini-va-calculator-75fde22feb83">[link to article]</a>.</p>
+      <br/><p>What's in it for you?</p>
+      <p>-- Free Mentorship: You'll receive invaluable mentorship directly from me, gaining insights and expertise that will set you on the path to success in your future endeavors.</p>
+      <p>-- Hands-On Experience: This is your chance to get hands-on experience with a groundbreaking project that's making waves in the tech world.</p>
+      <br/><p>Based in Toronto, I've already laid the foundation for the functional aspects of the va-calculator-v1.0. Now, I'm reaching out to YOU to join forces and elevate this project to new heights by adding those eye-catching decorative elements and styles that will make it truly shine.</p>
+      <br/><p>For a sneak peek into my development approach, check out <a href="https://drive.google.com/file/d/1DQ9U4dWI2uQM4nBpYycgD4v-wgvjJMGS/view">[this video]</a>, available in my VAOP group: eco-programming paradigm <a href="https://t.me/ecoprog">[https://t.me/ecoprog]</a>. It's a testament to the innovation and dedication that VAOP represents.</p>
+      <br/><p>Don't miss out on this incredible opportunity to make your mark on the future of technology. Let's collaborate, innovate, and create something extraordinary together. Reach out to me today, and let's get started on this exciting venture!</p>
+      <p></p>
   </div>
   );
   }
